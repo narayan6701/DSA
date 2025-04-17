3b36c80393ddfdc54428e3c1ea1129b88f45f3d0
Narayan Bhardwaj <narayan6701@outlook.in>
2025-04-17 14:13:53 +0530

    array definded, previously it was initialized

--- C/C - 3/addition_of_matrix.c
+++ C/C - 3/addition_of_matrix.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
 int main()
 {
-    int arr1[2][3] = {{1, 2, 3}, {4, 5, 6}};
-    int arr2[2][3] = {{7, 8, 9}, {10, 11, 12}};
-    
-    int result[2][3];
+    int arr1[2][3];
+    int arr2[2][3];
+    printf("Enter the value of martrix 1\n");
 
-    for(int i=0; i<2; i++){
-        for(int j=0; j<3; j++){
-            result[i][j] = arr1[i][j] + arr2[i][j]; 
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &arr1[i][j]);
         }
     }
 
-    for(int i=0; i<2; i++){
-        for(int j=0; j<3; j++){
+    printf("Enter the value of martrix 2\n");
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &arr2[i][j]);
+        }
+    }
+
+    int result[2][3];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            result[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             printf("%d ", result[i][j]);
         }
     }
