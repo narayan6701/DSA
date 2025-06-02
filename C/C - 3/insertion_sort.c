63a3cb6b22b081355041b03024a90edb3e4334e2
Narayan Bhardwaj <narayan6701@outlook.in>
2025-06-02 09:40:04 +0530

    updated code

--- C/C - 3/insertion_sort.c
+++ C/C - 3/insertion_sort.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-void insertion_sort(int arr[], int n) {
+void insertion_sort(int arr[], int n)
+{
     int i, key, j;
-    for (i = 1; i < n; i++) {
+    for (i = 1; i < n; i++)
+    {
         key = arr[i];
         j = i - 1;
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key)
+        {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
@@ -13,30 +16,35 @@ void insertion_sort(int arr[], int n) {
     }
 }
 
-void selection_sort(int arr[], int n) {
-    int i, j, min_idx, temp;
-    for (i = 0; i < n - 1; i++) {
-        min_idx = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
+void selection_sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+            {
                 min_idx = j;
             }
         }
-        temp = arr[min_idx];
+        int temp = arr[min_idx];
         arr[min_idx] = arr[i];
         arr[i] = temp;
     }
 }
 
-int main() {
+int main()
+{
     int arr[5] = {1, 4, 3, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
-    
+
     // Uncomment one of the following lines to use the desired sorting algorithm
     // insertion_sort(arr, n);
     selection_sort(arr, n);
-    
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
     return 0;
